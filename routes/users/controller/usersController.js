ff0e7e7d6be9fa278e7de1805efe5e827854919e
Gregory.Johnson <3531op@gmail.com>
2021-04-25 10:46:24 -0400

    signUp function works

--- routes/users/controller/usersController.js
+++ routes/users/controller/usersController.js
@@ -1,9 +1,30 @@
+const mongoErrorParser = require("../../lib/mongoErrorParser");
+const User = require("../model/User");
+const bcrypt = require("bcryptjs");
+
 module.exports = {
   signUp: async (req, res) => {
     try {
-      res.json("hello");
+      let salted = await bcrypt.genSalt(10);
+
+      let hashedPassword = await bcrypt.hash(req.body.password, salted);
+
+      let { firstName, lastName, email } = req.body;
+
+      let createdUser = new User({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+      });
+
+      let savedUser = await createdUser.save();
+
+      res.json({
+        data: savedUser,
+      });
     } catch (e) {
-      console.log(e);
+      res.status(500).json(mongoErrorParser(e));
     }
   },
 };
