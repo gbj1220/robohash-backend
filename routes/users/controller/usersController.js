c3d5c011eef550d02e656a14f3b5ec95d16dbc83
Gregory.Johnson <3531op@gmail.com>
2021-04-25 10:29:58 -0400

    signUP route works

---
+++ routes/users/controller/usersController.js
@@ -0,0 +1,9 @@
+module.exports = {
+  signUp: async (req, res) => {
+    try {
+      res.json("hello");
+    } catch (e) {
+      console.log(e);
+    }
+  },
+};
--- routes/users/usersRouter.js
+++ routes/users/usersRouter.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var router = express.Router();
 
+var { signUp } = require("./controller/usersController");
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
+router.post("/sign-up", signUp);
+
 module.exports = router;
-b;
